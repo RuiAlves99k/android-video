062c9855402c290e8fe212623841b2350cdbfd75
Rui Alves <ruialves99k@gmail.com>
2024-05-22 01:19:21 +0200

    add resources add player controls add models for video

---
+++ app/src/main/java/com/appsrui/video/ui/Extensions.kt
@@ -0,0 +1,11 @@
+package com.appsrui.video.ui
+
+import java.util.concurrent.TimeUnit
+
+fun Number.toFormattedDuration(timeUnit: TimeUnit = TimeUnit.SECONDS): String {
+    val thisLong = toLong()
+    return "%02d:%02d".format(
+        timeUnit.toMinutes(thisLong),
+        timeUnit.toSeconds(thisLong) % 60,
+    )
+}
---
+++ app/src/main/java/com/appsrui/video/ui/PlayerScreen.kt
@@ -0,0 +1,10 @@
+package com.appsrui.video.ui
+
+import androidx.compose.runtime.Composable
+import androidx.media3.common.Player
+import androidx.media3.session.SessionToken
+
+@Composable
+fun PlayerScreen(player: Player, token: SessionToken){
+
+}
